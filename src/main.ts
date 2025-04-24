a8f7621e570ffc29ebc107dad05424f07211d0dc
Zhang WenChao <zhangwenchao001@gmail.com>
2025-04-24 11:03:06 +0800

    feat: add swaager doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    defaultVersion: '1',
+    type: VersioningType.URI,
+  });
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addServer('http://localhost:3005')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: true,
+  });
+
+  SwaggerModule.setup('doc/api', app, document);
+
   await app.listen(process.env.PORT ?? 3005);
 }
 bootstrap().catch((err) => console.error('Err:', err));
