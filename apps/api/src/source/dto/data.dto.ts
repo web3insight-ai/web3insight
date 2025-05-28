614006c27d998c9597e431006919fa4715bb956e
Zhang WenChao <zhangwenchao001@gmail.com>
2025-05-28 10:38:12 +0800

    feat: remove rss3

--- apps/api/src/source/dto/data.dto.ts
+++ apps/api/src/source/dto/data.dto.ts
@@ -1,13 +1,11 @@
 export enum EcoType {
   ALL = 'ALL',
   NEAR = 'NEAR',
-  Aurora = 'Aurora',
   Solana = 'Solana',
   OpenBuild = 'OpenBuild',
   Starknet = 'Starknet',
   Bitcoin = 'Bitcoin',
   Ethereum = 'Ethereum',
-  NSL = 'Natural Selection Labs  (RSS3)',
   Aptos = 'Aptos',
   Sui = 'Sui',
   Mantle = 'Mantle',
