35c678088f0ded49aedd4e940c00994806c50380
Ourai Lin <ourairyu@gmail.com>
2025-05-30 13:17:15 +0800

    chore: reduce repository table columns of admin

--- src/domain/repository/views/repository-list/RepositoryList.tsx
+++ src/domain/repository/views/repository-list/RepositoryList.tsx
@@ -1,10 +1,28 @@
-import RepoTableWidget from "../../widgets/repo-table";
+import clsx from "clsx";
+import { Card } from "@nextui-org/react";
+
+import DataTable from "@/components/control/data-table";
+
+import { LinkReadFieldWidget } from "../../widgets/link-field";
 
 import type { RepositoryListViewWidgetProps } from "./typing";
 
-function RepositoryListView({ dataSource }: RepositoryListViewWidgetProps ) {
+function RepositoryListView({ className, dataSource }: RepositoryListViewWidgetProps ) {
   return (
-    <RepoTableWidget dataSource={dataSource} />
+    <Card className={clsx("bg-white dark:bg-gray-800 shadow-sm border-none hover:shadow-md transition-all duration-300", className)}>
+      <DataTable
+        dataSource={dataSource}
+        columns={[
+          { title: "#", key: "serialNumber", type: "index" },
+          {
+            title: "Repository",
+            key: "fullName",
+            span: 5,
+            render: (_, { row }) => <LinkReadFieldWidget value={row.fullName} />,
+          },
+        ]}
+      />
+    </Card>
   );
 }
 
