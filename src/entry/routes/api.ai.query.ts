ee9f387adfe449671d1443e0ff9b6463a69dc973
Ming <757352776@qq.com>
2025-06-06 12:31:47 +0800

    remove options

--- src/entry/routes/api.ai.query.ts
+++ src/entry/routes/api.ai.query.ts
@@ -2,15 +2,6 @@ import { fetchAnalyzedStatistics } from "~/ai/repository";
 import { createServerAction, createPreflightAction } from "../utils";
 
 export const action = createServerAction("POST", async ({ request }) => {
-  if (request.method === "OPTIONS") {
-    return new Response(null, {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-      },
-    });
-  }
-
   const formData = await request.formData();
   const res = await fetchAnalyzedStatistics({
     query: formData.get("query") as string,
