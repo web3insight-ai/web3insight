172927b83a92345d058c206c9d3c65267c4ecea8
Bystander2 <757352776@qq.com>
2025-06-06 13:14:08 +0800

    refactor: make AI query streamable
    * feat: update ai query
    
    * update
    
    * remove uuid
    
    * remove uuid
    
    * feat: change to streaming
    
    * fix
    
    * remove options

--- src/domain/ai/repository/client.ts
+++ src/domain/ai/repository/client.ts
@@ -1,4 +1,3 @@
-import HttpClient from "@/clients/http/HttpClient";
 import { createOpenAI } from "@ai-sdk/openai";
 
 const openai = createOpenAI({
@@ -6,12 +5,5 @@ const openai = createOpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const httpClient = new HttpClient({
-  baseUrl: `${process.env.AI_API_URL}/api/v1`,
-  headers: {
-    Authorization: `Bearer ${process.env.AI_API_TOKEN}`,
-  },
-});
 
 export { openai };
-export default httpClient;
