1d91abc1d58a5c787d8372073a12861ce9d91663
Zhang WenChao <zhangwenchao001@gmail.com>
2025-06-16 22:05:53 +0800

    feat: add github pool api

---
+++ src/api/controller/github.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { GithubService } from '../services/github.services';
+import { Request } from 'express';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { AppAuthGuard } from '@/auth/app.auth.guard';
+
+@Controller('github')
+export class GithubController {
+  constructor(private readonly githubService: GithubService) {}
+
+  @Get('proxy/*path')
+  @ApiBearerAuth()
+  @UseGuards(AppAuthGuard)
+  async getGitHub(@Req() req: Request) {
+    return this.githubService.get(req);
+  }
+
+  @Post('proxy/*path')
+  @ApiBearerAuth()
+  @UseGuards(AppAuthGuard)
+  async postGitHub(@Req() req: Request) {
+    return this.githubService.post(req);
+  }
+}
