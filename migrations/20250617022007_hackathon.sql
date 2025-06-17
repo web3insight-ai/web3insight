b88cc1c575f3a02df69e34b7bf899a3c576c464b
Zhang WenChao <zhangwenchao001@gmail.com>
2025-06-17 10:39:09 +0800

    feat: add user analysis

---
+++ migrations/20250617022007_hackathon.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS "api"."analysis_users"
+(
+    "id"              BIGSERIAL,
+    "intent"          TEXT                     NOT NULL DEFAULT 'hackathon',
+    "request_data"    JSONB                    NOT NULL DEFAULT '{}',
+    "github"          JSONB                    NOT NULL DEFAULT '{}',
+    "submitter_email" TEXT                     NOT NULL DEFAULT '',
+    "data"            JSONB                    NOT NULL DEFAULT '{}',
+    "created_at"      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    "updated_at"      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
