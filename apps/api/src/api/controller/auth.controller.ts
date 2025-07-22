c6a5e1ea05cebb753b4eab488fabec80bd4c77ef
Zhang WenChao <zhangwenchao001@gmail.com>
2025-07-22 12:58:11 +0800

    feat: add get user profile api

--- apps/api/src/api/controller/auth.controller.ts
+++ apps/api/src/api/controller/auth.controller.ts
@@ -1,13 +1,31 @@
-import { OAuthService } from '@/auth/services/oauth.services';
-import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from '@/auth/services/auth.services';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { LoginReqDto } from '../dto/api.dto';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { AppAuthGuard } from '@/auth/app.auth.guard';
+import { RequestWithUser } from '@/auth/auth.jwt.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly oauthServices: OAuthService) {}
+  constructor(private readonly authServices: AuthService) {}
 
   @Post('login/oauth/code')
+  @ApiOperation({
+    summary: 'Use oauth code to login',
+    description: '',
+  })
   async login(@Body() body: LoginReqDto) {
-    return this.oauthServices.login(body);
+    return this.authServices.login(body);
+  }
+
+  @Get('user')
+  @ApiOperation({
+    summary: 'Get user profile',
+    description: '',
+  })
+  @ApiBearerAuth()
+  @UseGuards(AppAuthGuard)
+  async getUser(@Req() req: RequestWithUser) {
+    return this.authServices.getUserInfo(req.user.uid);
   }
 }
